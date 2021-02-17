2fcc0b5b4e153705ee653e140868d742c90900af
rafael najjar <rafaelnajjar@gmail.com>
2021-02-17 11:04:26 -0300

    fixed position in a lot of places. Styled Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ButtonHatch from '../Hatch/ButtonHatch/ButtonHatch';
 import InputHatch from '../Hatch/InputHatch/InputHatch';
+import './Form.css';
 
 const Form = (props) => {
     const handleChange = (value, field) => {
@@ -12,29 +13,35 @@ const Form = (props) => {
     }
 
     return (
-        <>
+        <div class="formContainer">
             {props.selectedLayout &&
             <ButtonHatch class="config" key="edit" onClick={() => props.setModalLayout(true)}>
                 Edit this card layout
             </ButtonHatch>
             }
 
-            {
-            props.allInfo[props.selectedGame] &&
-            (props.allInfo[props.selectedGame][props.selectedLayout] 
-            ? 
-            props.allInfo[props.selectedGame][props.selectedLayout].fields.map((i) => {
-                return(
-                    <div key={`field-${i.label}`}>
-                        <label>{`${i.label}`}</label>
-                        <InputHatch onChange={(e) => handleChange(e.target.value, i.label)} value={props.cardForm[i.label]} />
-                    </div>
-                )
-            })
-            : 
-            <></>)
-            }
-        </>
+            <div class="fieldsContainer">
+                {
+                props.allInfo[props.selectedGame] &&
+                (props.allInfo[props.selectedGame][props.selectedLayout] 
+                ? 
+                props.allInfo[props.selectedGame][props.selectedLayout].fields.map((i) => {
+                    return(
+                        <div key={`field-${i.label}`} class="fieldContainer">
+                            <div class="field-label">
+                                {`${i.label}`}
+                            </div>
+                            <div class="field-input">
+                                <InputHatch onChange={(e) => handleChange(e.target.value, i.label)} value={props.cardForm[i.label]} />
+                            </div>
+                        </div>
+                    )
+                })
+                : 
+                <></>)
+                }
+            </div>
+        </div>
     );
 };
 
